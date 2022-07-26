6c258776b6b3025bbc64c3fb633820dbf408a8d1
s0-p <b00v0429@gmail.com>
2022-07-26 13:48:00 +0900

    replace H2 with MySQL

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -19,19 +19,9 @@ public class MemberService {
 
     public Long join(Member member) {
 
-        validateDuplicateMember(member);    //  중복 회원 검증******
         memberRepository.save(member);
         return member.getId();
     }
-
-    //  중복 회원 검증 *********************************
-    private void validateDuplicateMember(Member member) {
-        memberRepository.findByName(member.getName())
-                .ifPresent(m -> {
-                    throw new IllegalStateException("이미 존재하는 회원입니다.");
-                });
-    }
-
 
     public List<Member> findMembers() {
         return memberRepository.findAll();
